c83a20ae3c90a2db03419203190d0d05a670dd8d
andrebtw <gwandrewgd@gmail.com>
2022-02-17 20:08:57 +0100

    Finished the home page.

---
+++ index.js
@@ -0,0 +1,13 @@
+// Navigation between websites
+
+function nav_DAC () {
+    window.open("dac.html","_self");
+}
+
+function nav_ADC () {
+    window.open("adc.html","_self");
+}
+
+function nav_Home () {
+    window.open("index.html","_self");
+}
