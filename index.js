7e29239cc62fe8bf789a8df1b36eedaa7c0615a9
andrebtw <gwandrewgd@gmail.com>
2022-03-18 13:31:53 +0100

    Finished the explanation and added the converter for the DAC

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// Navigation between pages
+// On load
 
+
+// Navigation between pages
 function nav_DAC () {
     document.getElementById('dac').scrollIntoView();
 }
@@ -11,3 +13,24 @@ function nav_ADC () {
 function nav_Home () {
     document.getElementsByClassName('welcome').scrollIntoView();
 }
+
+
+// Convertion DAC
+function getDAC() {
+    const vmin = document.getElementById('vmin').value;
+    const vmax = document.getElementById('vmax').value;
+    const res = document.getElementById('res').value;
+    const value = document.getElementById('value').value;
+    var maxvalue = (2**parseInt(res)) - 1;
+    var Ve = parseInt(vmax) - parseInt(vmin);
+    var Q = Ve / maxvalue;
+    var quantum = Ve/(2**res - 1);
+    var Vout = quantum * parseInt(value) + vmin;
+    
+    var quantum_text = document.getElementById('quantum');
+    var vout_text = document.getElementById('vout');
+    quantum_text.innerHTML = '<b>Q = ' + Ve + ' / (2<sup>' + res + '</sup> - 1) = ' + quantum + ' V</b>';
+    vout_text.innerHTML = '<b>V<sub>out</sub> = ' + quantum + ' x ' + value + ' = ' + Vout + ' V</b>'
+
+}
+
